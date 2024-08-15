15d1a17a2380fc2cd72bf1795d9e8073e45198c4
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-08-15 22:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const userConfirmed = confirm("Do you want to proceed?");
+if (userConfirmed) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
